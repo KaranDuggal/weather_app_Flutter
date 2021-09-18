e66c13932614fe033a1a0ee19e2391b6be2365c4
KaranDuggal <duggalkaran400@gmail.com>
2021-09-18 12:41:41 +0530

    location access

--- lib/src/screen/home_screen.dart
+++ lib/src/screen/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
-
+import 'package:geolocator/geolocator.dart';
+import 'package:flutter_geocoder/geocoder.dart';
 class HomeScreen extends StatefulWidget {
   const HomeScreen({ Key? key }) : super(key: key);
 
@@ -10,10 +11,42 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
 
+  var weatherInfo;
+  var weatherOldInfo;
+  bool apiHit = false;
+  String search = '';
+  String sunrise = '';
+  String sunset = '';
+  String defaltCity = 'jalandhar';
   @override
   void initState(){
     super.initState();
+    _determinePosition();
   }
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    await Geolocator.openLocationSettings();
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+    permission = await Geolocator.checkPermission();
+    await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {return Future.error('Location permissions are denied');}
+    }
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error('Location permissions are permanently denied, we cannot request permissions.');
+    } 
+    Position coordinates = await Geolocator.getCurrentPosition();
+    var addresses = await Geocoder.local.findAddressesFromCoordinates(Coordinates(coordinates.latitude, coordinates.longitude));
+    return coordinates;
+  }
+
+
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
