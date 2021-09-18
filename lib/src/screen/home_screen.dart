05fb0680d22e6d15586c8bf04b9e9523edd1a478
KaranDuggal <duggalkaran400@gmail.com>
2021-09-18 13:52:07 +0530

    get data sussesssully

--- lib/src/screen/home_screen.dart
+++ lib/src/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:math';
 
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
@@ -61,14 +62,11 @@ class _HomeScreenState extends State<HomeScreen> {
       // await Future.delayed(Duration(seconds: 2));dd MMMM yyyy, hh:mm a
       sunrise = DateFormat('hh:mm a').format(DateTime.fromMillisecondsSinceEpoch(weatherInfo.sys.sunrise *1000));
       sunset = DateFormat('hh:mm a').format(DateTime.fromMillisecondsSinceEpoch(weatherInfo.sys.sunset *1000));
-      print('sunrise $sunrise , sunset $sunset ');
       setState(() {
-
       });
     }else{
       weatherInfo = weatherOldInfo;
       setState(() {
-
       });
       CoolAlert.show(
         context: context,
@@ -82,12 +80,232 @@ class _HomeScreenState extends State<HomeScreen> {
   
   @override
   Widget build(BuildContext context) {
+    var cityNames = ["Mumbai","Delhi","Chennai","Jalandhar","Hyderabad","Pune","Berlin","Chicago","Washington","Las Vegas","New York","Munich","Hamburg"];
+    final _random = Random();
+    var city = cityNames[_random.nextInt(cityNames.length)];
     return Scaffold(
       body: StreamBuilder(
         stream: Connectivity().onConnectivityChanged,
         builder: (BuildContext contaxt, AsyncSnapshot<ConnectivityResult> snapshot){
           if(snapshot.hasData && snapshot.data != ConnectivityResult.none){
-            return apiHit ? const Center(child: Text("connected")) : const Center(child: Text("fetching data"));
+            return apiHit 
+            ? 
+            SingleChildScrollView(
+              child: SafeArea(
+                // BackGround
+                child: Container(
+                  height: MediaQuery.of(context).size.height,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topRight,
+                      end: Alignment.bottomCenter,
+                      stops: const [
+                        0.1,
+                        0.9
+                      ],
+                      colors: [
+                        Colors.blue.shade300,
+                        Colors.orange.shade300
+                      ]
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      Container( // search bar
+                        // color: Colors.grey,
+                        padding: const EdgeInsets.all(8),
+                        margin: const EdgeInsets.symmetric(
+                          horizontal: 14,
+                          vertical: 24
+                        ),
+                        decoration: BoxDecoration(
+                          color: Colors.blue.shade100,
+                          borderRadius: BorderRadius.circular(50)
+                        ),
+                        child: Row(
+                          children: [
+                            const SizedBox(width:10),
+                            GestureDetector(
+                              onTap: () async {
+                                if(search.isNotEmpty){
+                                  apiHit = false;
+                                  setState(() {
+                                  });
+                                  await getData(search);
+                                }
+                              },
+                              child: const Icon(Icons.search),
+                            ),
+                            const SizedBox(width:10),
+                            Expanded( 
+                              child: TextFormField(
+                                keyboardType: TextInputType.text,
+                                onFieldSubmitted: (value) async{
+                                  if(value.isNotEmpty){
+                                    apiHit = false;
+                                    setState(() {
+                                    });
+                                    await getData(search);
+                                  }
+                                },
+                                decoration: InputDecoration(
+                                  hintText: "Search any city name eg. $city",
+                                  border: InputBorder.none
+                                ),
+                                onChanged: (value){
+                                  search = value;
+                                },
+                              )
+                            )
+                          ],
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              margin: const EdgeInsets.symmetric(horizontal: 20),
+                              padding: const EdgeInsets.all(25),
+                              decoration: BoxDecoration(
+                                color: Colors.white.withOpacity(0.5),
+                                borderRadius: BorderRadius.circular(15)
+                              ),
+                              child: Row(
+                                children: [
+                                  Image.network("https://openweathermap.org/img/wn/${weatherInfo.weather[0].icon}.png"),
+                                  // Image.asset('assets/images/snowyoutline.png',height: 50,width: 50,),
+                                  const SizedBox(width: 10,height: 50,),
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "${weatherInfo.weather[0].description}"
+                                      ),
+                                      Text(
+                                        "in ${weatherInfo.name}, ${weatherInfo.sys.country}"
+                                      ),
+                                    ],
+                                  ),
+                                  Expanded(
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: [
+                                        Column(
+                                          crossAxisAlignment: CrossAxisAlignment.end,
+                                          children: [
+                                            Text(
+                                              "sunrise :- $sunrise"
+                                            ),
+                                            Text(
+                                              "sunset :-  $sunset"
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20,),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              height: 200,
+                              margin: const EdgeInsets.symmetric(horizontal: 20),
+                              padding: const EdgeInsets.all(25),
+                              decoration: BoxDecoration(
+                                color: Colors.white.withOpacity(0.5),
+                                borderRadius: BorderRadius.circular(15)
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children:[
+                                  Image.asset('assets/images/temp.png',height: 50,width: 50,),
+                                  // Image.network("https://toppng.com/uploads/preview/temperature-free-icon-temperature-icon-free-11553469636smayyrvo21.png",height: 50,width: 50,),
+                                  Row(
+                                    mainAxisAlignment : MainAxisAlignment.center,
+                                    children: [
+                                      Text("${weatherInfo.main.temp}",style: const TextStyle(fontSize: 70) ,),
+                                      const Text("C",style: TextStyle(fontSize: 40))
+                                    ],
+                                  )
+                                ]
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Expanded(
+                            child: Container(
+                              height: 150,
+                              margin: const EdgeInsets.fromLTRB(20 , 0, 10, 0),
+                              padding: const EdgeInsets.all(25),
+                              decoration: BoxDecoration(
+                                color: Colors.white.withOpacity(0.5),
+                                borderRadius: BorderRadius.circular(15)
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children:[
+                                  Image.asset('assets/images/wind.png',height: 30,width: 30,),
+                                  Row(
+                                    mainAxisAlignment : MainAxisAlignment.center,
+                                    children: [
+                                      Text("${weatherInfo.wind.speed}",style: const TextStyle(fontSize: 20) ,),
+                                      const SizedBox(width: 5,),
+                                      const Text("Km/hr",style: TextStyle(fontSize: 15))
+                                    ],
+                                  )
+                                ]
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              height: 150,
+                              margin: const EdgeInsets.fromLTRB(10 , 0, 20, 0),
+                              padding: const EdgeInsets.all(25),
+                              decoration: BoxDecoration(
+                                color: Colors.white.withOpacity(0.5),
+                                borderRadius: BorderRadius.circular(15)
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children:[
+                                  Image.asset('assets/images/humidity.png',height: 30,width: 30,),
+                                  Row(
+                                    mainAxisAlignment : MainAxisAlignment.center,
+                                    children: [
+                                      Text("${weatherInfo.main.humidity}",style: const TextStyle(fontSize: 20) ,),
+                                      const SizedBox(width: 5,),
+                                      const Text("Percent",style: TextStyle(fontSize: 15))
+                                    ],
+                                  )
+                                ]
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20,),
+                      const Text("Data Provided By Openweathermap.org"),
+                      // SizedBox(height: 100,),
+                    ],
+                  ),
+                )
+              ),
+            )
+            : 
+            const Center(child: Text("fetching data"));
           }else{
             return const Center(child: Text("Not connected"));
           }
